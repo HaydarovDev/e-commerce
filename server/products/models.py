5499fbaad2ddaf3b57162d5c1769e202d6209d10
HaydarovDev <asadbekcoder1@gmail.com>
2025-12-16 09:40:16 +0500

    feat: product details is added

--- server/products/models.py
+++ server/products/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Product (models.Model):
     title = models.CharField(max_length=200)
-    price = models.IntegerField() 
+    desc = models.TextField()
+    price = models.IntegerField()
+    count = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products/", null=True, blank=True)
 
     def __str__(self):
         return self.title
