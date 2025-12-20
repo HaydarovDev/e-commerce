6d7f58b3d3afb379f88c9512659ee904bf72bc28
HaydarovDev <asadbekcoder1@gmail.com>
2025-12-20 12:20:28 +0500

    feat: header added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 
-export default function page() {
+export default function Home() {
   return (
     <>
       <Header />
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,16 +1,45 @@
+import FlashIcon from "@/assets/images/icons/FlashIcon";
+import LikedIcon from "@/assets/images/icons/LikedIcon";
 import SearchIcon from "@/assets/images/icons/SearchIcon";
+import UserIcon from "@/assets/images/icons/UserIcon";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header>
-      <Link href="">Market</Link>
-      <ul>
-        <li>Products</li>
-        <li>Contact Us</li>
-        <label htmlFor="">
+    <header className="flex p-2 justify-between shadow-sm">
+      <Link href="/" className="flex text-2xl font-bold items-center">
+        <FlashIcon />
+        Market
+      </Link>
+      <ul className="flex items-center gap-5 font-semibold">
+        <Link href="/products" className="hidden md:block">
+          Products
+        </Link>
+        <Link href="" className="hidden md:block">
+          Contact Us
+        </Link>
+        <label
+          htmlFor="search"
+          className="flex items-center border p-1 rounded gap-2 mx-3"
+        >
           <SearchIcon />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="outline-0"
+            id="search"
+          />
         </label>
+        <button aria-label="wishlist">
+          <Link href="">
+            <LikedIcon />
+          </Link>
+        </button>
+        <button aria-label="account">
+          <Link href="">
+            <UserIcon />
+          </Link>
+        </button>
       </ul>
     </header>
   );
